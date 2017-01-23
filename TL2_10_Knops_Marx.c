88dae2ef4f178005c4cb4a520cdc9f1c7c314fc5
w00000t <w00t.gaming@gmx.de>
2017-01-23 09:16:27 +0100

    new menu
    menu für die standardkonfigurationen

--- TL2_10_Knops_Marx.c
+++ TL2_10_Knops_Marx.c
@@ -34,11 +34,30 @@
 #define PREIS_HDD 130.95
 #define PREIS_GPU 319.13
 
-// globale Variable für den Warenkorb
-float fWarenkorb = 0.0;
+#define MENU_INDENTATION_WHITESPACES 4
 
-int main(void){
+/*
+* Funktionsprototypen
+*/
+void printMenuStandartkonfiguration(void);
+void printWarenkorbForMenu(int iMaxMenuLength);
+void printLine(int iLength);
+void printLineBreaks(int iLineBreaks);
+void printTabs(int iTabs);
+void printWhitespaces(int iWhitespaces);
+
+// globale Variable für den Warenkorb
+float fWarenkorb = 22320.0;
+
+int main(void)
+{
   int iSuccessfullInput = 0;
+
+  printMenuStandartkonfiguration();
+
+  system("pause");
+
+  return 0;
 }
 
 /*
@@ -46,10 +65,112 @@ int main(void){
 * Input:      -
 * Output:     -
 */
-void printMenuStandartkonfiguration(void){
+void printMenuStandartkonfiguration(void)
+{
+  // Warenkorb
+  printLineBreaks(3);// space top
+  printWarenkorbForMenu(46);// Warenkorb anzeigen
+  printLineBreaks(2); // space Warenkorb <-> Menu
 
+  // Menu headline
+  printWhitespaces(MENU_INDENTATION_WHITESPACES);
+  printf("S T A N D A R D K O N F I G U R A T I O N E N");
+  printLineBreaks(1);
+  printWhitespaces(MENU_INDENTATION_WHITESPACES);
+  printLine(45);
+  printLineBreaks(1);
+
+  // Auswahlmöglichkeiten
+  printWhitespaces(MENU_INDENTATION_WHITESPACES);
+  printf("1: Fun Star");
+  printWhitespaces(22);
+  printf("%7.2f Euro", PREIS_FUN_STAR);
+  printLineBreaks(1);
+
+  printWhitespaces(MENU_INDENTATION_WHITESPACES);
+  printf("2: High End");
+  // Leerzeichen zwischen Bezeichnung und Preis
+  printWhitespaces(22);
+  printf("%7.2f Euro", PREIS_HIGH_END);
+  printLineBreaks(1);
+
+  printWhitespaces(MENU_INDENTATION_WHITESPACES);
+  printf("3: Multimedia Star");
+  // Leerzeichen zwischen Bezeichnung und Preis
+  printWhitespaces(15);
+  printf("%7.2f Euro", PREIS_MULTIMEDIA_STAR);
+  printLineBreaks(1);
+
+  printWhitespaces(MENU_INDENTATION_WHITESPACES);
+  printf("4: Office Star");
+  // Leerzeichen zwischen Bezeichnung und Preis
+  printWhitespaces(19);
+  printf("%7.2f Euro", PREIS_OFFICE_STAR);
+  printLineBreaks(1);
+
+  printWhitespaces(MENU_INDENTATION_WHITESPACES);
+  printf("0: Abbruch");
+  printLineBreaks(1);
 }
 
+/*
+* Funktion:   printTabs
+* Input:      int iTabs
+* Output:     -
+*/
+void printTabs(int iTabs)
+{
+  int i;
+
+  for(i = 0;i < iTabs;i++)
+  {
+    printf("\t");
+  }
+}
+
+/*
+* Funktion:   printWhitespaces
+* Input:      int iWhitespaces
+* Output:     -
+*/
+void printWhitespaces(int iWhitespaces)
+{
+  int i;
+
+  for(i = 0; i < iWhitespaces; i++)
+  {
+    printf(" ");
+  }
+}
+
+/*
+* Funktion:   printWarenkorbForMenu
+* Input:      -
+* Output:     -
+*/
+void printWarenkorbForMenu(int iMaxMenuLength)
+{
+  int iStartPos = iMaxMenuLength + MENU_INDENTATION_WHITESPACES - 25;
+
+  printWhitespaces(iStartPos);
+  printf("Warenkorb: ");
+  printf("%7.2f Euro", fWarenkorb);
+}
+
+/*
+* Funktion:   printLineBreaks
+* Input:      int iLinebreaks
+* Output:     -
+*/
+void printLineBreaks(int iLinebreaks)
+{
+  int i;
+  
+  for(i = 0;i < iLinebreaks;i++)
+  {
+    printf("\n");
+  }
+}
 /*
 * Funktion:   printLine
 * Input:      int iLength
@@ -57,10 +178,12 @@ void printMenuStandartkonfiguration(void){
 *
 * Die Funktion gibt eine Linie, bestehend aus "=", mit der Länge iLength aus.
 */
-void printLine(int iLength){
+void printLine(int iLength)
+{
   int i;
 
-  for(i = 0; i < iLength; i++){
+  for(i = 0; i < iLength; i++)
+  {
     printf("=");
   }
 
